8ba851c7796223871d45f84ce4c7a01fc6b8aa98
Ayush Kumar <68886542+Hi-Ayush@users.noreply.github.com>
2020-10-21 21:00:02 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  render() {
+    const handleClick = function () {
+      const text =
+        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+      return text;
+    };
+    return (
+      <div id="main">
+        <button id="click" onClick={handleClick}>
+          Click Me
+        </button>
+        {this.text}
+        {/* Do not remove this main div!! */}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-

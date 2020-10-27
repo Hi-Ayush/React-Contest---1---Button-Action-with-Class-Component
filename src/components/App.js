1661685731c25db4c75c42eb8476b9bebce69b6d
Ayush Kumar <68886542+Hi-Ayush@users.noreply.github.com>
2020-10-27 09:46:58 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,28 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isRenderPara: false
+    };
+    // this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick = () => {
+    this.setState({ isRenderPara: true });
+  };
 
   render() {
     return (
       <div id="main">
-        <button
-          id="click"
-          onClick={() => (
-            <p>
-              Hello, I've learnt to use the full-stack evaluation tool. This
-              makes me so happy
-            </p>
-          )}
-        >
+        <button id="click" onClick={this.handleClick}>
           Click Me
         </button>
+        {this.state.isRenderPara && (
+          <p>
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
       </div>
     );
   }
